60e1807be56f482755be1631dbb7a2484d136717
Zaman-R <zaman.r.official@gmail.com>
2023-10-21 12:44:53 +0600

    Post Method Added

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
 
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+// create a post method for the server  
 
 
 
@@ -28,19 +29,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productsCollection = client.db('productDB').collection('product');
+
+
+
+    app.post('/products',async(req,res)=>{
+        const newProduct = req.body;
+        console.log(newProduct);
+        const result = await productsCollection.insertOne(newProduct);
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
-    res.send("Coffee Running");
+    res.send("Server Running");
 })
 
 
